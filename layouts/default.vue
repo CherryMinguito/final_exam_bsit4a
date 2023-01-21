ab97eda02d56f460acdea3e1421a0effe7491b2f
Ian Subingsubing <ian.subingsubing@ctu.edu.ph>
2023-01-21 21:56:51 +0800

    Questionnaire

--- layouts/default.vue
+++ layouts/default.vue
@@ -106,6 +106,11 @@ export default {
           icon: 'mdi-chart-bubble',
           title: 'Inspire',
           to: '/inspire'
+        },
+        {
+          icon: 'mdi-chat-question',
+          title: 'Questionnaire',
+          to: 'questionnaire'
         }
       ],
       miniVariant: false,
